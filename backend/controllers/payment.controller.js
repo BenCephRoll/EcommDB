a0d79dd5631e8a52b1a64c64af816397f2a0a645
BenCephRoll <benceph389@gmail.com>
2024-12-19 21:18:57 +0100

    Update payment.controller.js

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -108,7 +108,7 @@ export const checkoutSuccess = async (req, res) => {
 			console.error(req.body.transaction.data.metadata)
 
 			    // Assuming the userId is passed via metadata in the transaction
-    const userId = req.body.transaction.data.metadata.userId;
+    const userId = transaction.data.metadata.userId;
 
     // Find the user by ID in the database
     const userfind = await User.findById(userId);
